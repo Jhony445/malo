9e89fa8e2a35ae28050dbeaed99cb910c902bef3
GAma131 <gamaliel.g.g131@gmail.com>
2024-10-24 12:05:35 -0600

    Inserts empresa usuario

--- src/app/auth/features/register-company/register-company.component.ts
+++ src/app/auth/features/register-company/register-company.component.ts
@@ -1,26 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoaderComponent } from '../../../shared/ui/loader/loader.component';
+import { NotificationComponent } from '../../../shared/ui/notification/notification.component';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, LoaderComponent, NotificationComponent],
   templateUrl: './register-company.component.html',
   styleUrls: ['./register-company.component.css'],
 })
 export class RegisterCompany {
   currentStep = 1;
-
-  // Actualización de las propiedades
+  
   nombre = '';
   industria = '';
   ubicacion = '';
   email = '';
   contrasena = '';
   confirmarContrasena = '';
+
   router = inject(Router);
+  http = inject(HttpClient); // Inyecta HttpClient directamente
+  isLoading: boolean = false;
+
+  errorMessage: string = '';
 
   nextStep() {
     if (this.currentStep < 3) {
@@ -38,25 +45,52 @@ export class RegisterCompany {
     this.currentStep = step;
   }
 
-  // Validación del Formulario 1
   isForm1Valid(): boolean {
     return !!this.nombre && !!this.industria && !!this.ubicacion;
   }
 
-  // Validación del Formulario 2
   isForm2Valid(): boolean {
     return !!this.email && !!this.contrasena && this.contrasena === this.confirmarContrasena;
   }
 
   finishRegister() {
-    if(this.isForm1Valid() && this.isForm2Valid()){
-      this.currentStep++;
-    }else{
+    if (this.isForm1Valid() && this.isForm2Valid()) {
+      const empresaData = {
+        nombre: this.nombre,
+        industria: this.industria,
+        ubicacion: this.ubicacion,
+        contrasena: this.contrasena,
+        email: this.email,
+      };
+
+      this.isLoading = true;
+      // Hacer el POST directamente en el componente
+      this.http.post('https://malo-backend-empresas.onrender.com/api/Empresa/agregar-empresa', empresaData)
+        .subscribe({
+          next: (response) => {
+            this.isLoading = false;
+            console.log('Empresa registrada con éxito', response);
+            this.currentStep++; // Avanzar al siguiente paso si el registro es exitoso
+          },
+          error: (error) => {
+            this.isLoading = false;
+            this.errorMessage = "La empresa ya existe"
+            console.error('Error al registrar la empresa', error);
+            this.clearMessages();
+          }
+        });
+    } else {
       this.currentStep = 1;
     }
   }
 
-  goToSignIn(){
+  clearMessages() {
+    setTimeout(() => {
+      this.errorMessage = '';
+    }, 3000);  // Los mensajes desaparecen después de 3 segundos
+  }
+
+  goToSignIn() {
     this.router.navigate(['/auth/login']);
   }
 }
--- src/app/auth/features/sign-up/sign-up.component.ts
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -2,11 +2,14 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { LoaderComponent } from '../../../shared/ui/loader/loader.component';
+import { NotificationComponent } from '../../../shared/ui/notification/notification.component';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, LoaderComponent, NotificationComponent],
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css'],
 })
@@ -23,7 +26,11 @@ export class SignUpComponent {
   correo = '';
   contrasena = '';
   confirmPass = '';
+
   router = inject(Router);
+  http = inject(HttpClient);
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
   nextStep() {
     if (this.currentStep < 5) {
@@ -59,10 +66,46 @@ export class SignUpComponent {
   
   finishRegister() {
     if(this.isForm1Valid() && this.isForm2Valid() && this.isForm3Valid() && this.isForm4Valid()){
-      this.currentStep++;
+      const userData ={
+        nombre: this.nombre,
+        apellido: this.apellidos,
+        email: this.correo,
+        contrasena: this.confirmPass,
+        fecha_nacimiento: this.fechaNacimiento,
+        telefono: this.telefono,
+        estado_id: 1,
+        municipio_id: 1,
+        localidad_id: 1,
+        habilidades: '',
+        descripcion: '',
+        imagen_perfil: '',
+      };
+
+      this.isLoading = true;
+      // Hacer el POST directamente en el componente
+      this.http.post('https://malo-backend.onrender.com/api/Usuario/InsertarUsuario', userData)
+        .subscribe({
+          next: (response) => {
+            this.isLoading = false;
+            console.log('Usuario registrado con éxito', response);
+            this.currentStep++; // Avanzar al siguiente paso si el registro es exitoso
+          },
+          error: (error) => {
+            this.isLoading = false;
+            this.errorMessage = "Error al registrar usuario"
+            console.error('Error al registrar la empresa', error);
+            this.clearMessages();
+          }
+        });
     }else{
       this.currentStep = 1;
     }
+  }
+
+  clearMessages(){
+    setTimeout(() => {
+      this.errorMessage = '';
+    }, 3000)
   }
 
   goToSignIn(){
