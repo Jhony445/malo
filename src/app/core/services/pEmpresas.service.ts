730a401dc22212ef47058378292325b4f9c3dc91
Jonathan Quistian <yoni200108@gmail.com>
2024-11-08 19:31:25 -0600

    Cambios de Gama en perfiles

--- src/app/core/services/pEmpresas.service.ts
+++ src/app/core/services/pEmpresas.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserService } from './user.service';
 
@@ -11,21 +11,23 @@ export class PEmpresasService {
     private userService = inject(UserService);
     
     obtenerEmpresaPorId(): Observable<any> {
-        const empresaData = this.userService.getUserData(); // Método para obtener los datos del usuario
-        const url = 'https://malo-backend-empresas.onrender.com/api/Empresa/GetEmpresaPorId';
-        const empresaId = empresaData.sub; // Asumiendo que el ID de la empresa está en 'sub'
-    
-        // Enviar solo el ID, no todo el objeto
-        const requestBody = { id: empresaId };
-    
-        return this.http.post<any>(url, requestBody);
-    }
-    
+      const empresaData = this.userService.getUserData(); // Método para obtener los datos del usuario
+      const url = 'https://malo-backend-empresas.onrender.com/api/Empresa/GetEmpresaPorId';
+      const requestBody = JSON.stringify(empresaData.sub); // Convierte el ID directamente en una cadena JSON
+      
+      return this.http.post<any>(url, requestBody, {
+          headers: new HttpHeaders({
+              'Content-Type': 'application/json',
+              'accept': 'application/json'
+          })
+      });
+  }
+  
 
-      actualizarEmpresa(empresaData: any): Observable<any> {
-        const url = 'https://malo-backend-empresas.onrender.com/api/Empresa/actualizar-empresa';
-        
-        // Enviar los datos de la empresa en el cuerpo de la solicitud
-        return this.http.post<any>(url, empresaData);
-      }      
+    actualizarEmpresa(empresaData: any): Observable<any> {
+    const url = 'https://malo-backend-empresas.onrender.com/api/Empresa/actualizar-empresa';
+    
+    // Enviar los datos de la empresa en el cuerpo de la solicitud
+    return this.http.post<any>(url, empresaData);
+    }      
 }
