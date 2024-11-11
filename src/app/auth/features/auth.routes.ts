b39cc139834c6a5e1c9cd08b9b440a5b00c36686
Jonathan Quistian <yoni200108@gmail.com>
2024-11-11 11:41:48 -0600

    Vista de cuenta confirmada

--- src/app/auth/features/auth.routes.ts
+++ src/app/auth/features/auth.routes.ts
@@ -4,11 +4,13 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { SelectRegisterComponent } from './select-register/select-register.component';
 import { RegisterCompanyComponent } from './register-company/register-company.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { CuentaConfirmadaComponent } from './cuenta-confirmada/cuenta-confirmada.component';
 
 export const AUTH_ROUTES: Routes = [
   { path: 'login', component: LogInComponent },
   { path: 'select-register', component: SelectRegisterComponent },
   { path: 'register-company', component: RegisterCompanyComponent },
   { path: 'sign-up', component: SignUpComponent },
-  {path: 'forgot-password/cambiar-contrasena', component: ForgotPasswordComponent}
+  {path: 'forgot-password/cambiar-contrasena', component: ForgotPasswordComponent},
+  {path: 'cuenta-confirmada', component: CuentaConfirmadaComponent}
 ];
---
+++ src/app/auth/features/cuenta-confirmada/cuenta-confirmada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuenta-confirmada',
+  standalone: true,
+  imports: [],
+  templateUrl: './cuenta-confirmada.component.html',
+  styleUrl: './cuenta-confirmada.component.css'
+})
+export class CuentaConfirmadaComponent {
+
+}
