b65bb1b3f928bf47c5b7642ef1eeea1c12bd5633
Jonathan Quistian <yoni200108@gmail.com>
2024-11-13 11:48:18 -0600

    Corrección al cargar el dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,15 @@
-import { Routes } from '@angular/router'; 
-import { DashboardComponent } from './dashboard/dashboard.component'; 
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsuariosComponent } from './empleos/usuarios/usuarios.component';
-import { empresasComponent } from './empleos/empresas/empresas.component'; 
-
-//import { UserGuard } from './core/guards/user.guard';
+import { empresasComponent } from './empleos/empresas/empresas.component';
 import { EmpresaGuard } from './core/guards/empresa.guard';
 
 export const appRoutes: Routes = [
-  { path: '', component: DashboardComponent },  
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Redirige la raíz a 'dashboard'
+  { path: 'dashboard', component: DashboardComponent },  // Carga el componente de Dashboard
   {
     path: 'auth',
-    loadChildren: () => import('./auth/features/auth.routes').then(m => m.AUTH_ROUTES)  // Lazy load para auth
+    loadChildren: () => import('./auth/features/auth.routes').then(m => m.AUTH_ROUTES)
   },
   {
     path: 'usuario',
@@ -23,5 +22,5 @@ export const appRoutes: Routes = [
     loadChildren: () => import('./empleos/empresas/empresas.routes').then(m => m.empresasRoutes),
     canActivate: [EmpresaGuard]
   },
-  { path: '**', redirectTo: '' }  
+  { path: '**', redirectTo: 'dashboard' }  // Redirige cualquier ruta no encontrada a 'dashboard'
 ];
