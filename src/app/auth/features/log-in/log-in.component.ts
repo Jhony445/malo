a0d12198655223d753c00f29589d8f420ad5bd5e
Jonathan Quistian <yoni200108@gmail.com>
2024-11-26 18:42:06 -0600

    Error resuelto

--- src/app/auth/features/log-in/log-in.component.ts
+++ src/app/auth/features/log-in/log-in.component.ts
@@ -1,4 +1,136 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../../core/services/user.service';
+import { CommonModule } from '@angular/common';
+import { NotificationComponent } from '../../../shared/ui/notification/notification.component';
+import { LoaderComponent } from '../../../shared/ui/loader/loader.component';
 
+@Component({
+  selector: 'app-log-in',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, NotificationComponent, LoaderComponent],
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css']
+})
+export class LogInComponent {
+
+  loginForm: FormGroup;
+  errorMessage = '';
+  successMessage = '';
+  isLoading = false;
+  isCompanyLogin = false;
+  forgotPasswordMode = false;
+
+  http = inject(HttpClient);
+  router = inject(Router);
+  userService = inject(UserService);
+  fb = inject(FormBuilder);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      const loginApiUrl = this.isCompanyLogin
+        ? "https://malo-backend-empresas.onrender.com/api/Auth/login"
+        : "https://malo-backend.onrender.com/api/auth/login";
+
+      this.http.post(loginApiUrl, this.loginForm.value).subscribe(
+        (res: any) => {
+          this.isLoading = false;
+          if (res.result) {
+            localStorage.setItem('authToken', res.token);
+            this.userService.setAuthenticationState(true);
+            const decodedToken = this.userService.getUserData();
+            this.router.navigate([decodedToken.rol === 'Empresa' ? '/empresa' : '/usuario']);
+          } else {
+            this.errorMessage = "Credenciales inválidas. Por favor, verifique su correo y contraseña.";
+            this.clearMessages();
+          }
+        },
+        (error: HttpErrorResponse) => this.handleLoginError(error)
+      );
+    } else {
+      this.errorMessage = "Por favor, completa el formulario correctamente.";
+      this.clearMessages();
+    }
+  }
+
+  toggleForgotPassword() {
+    this.forgotPasswordMode = !this.forgotPasswordMode;
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    this.isCompanyLogin = false;
+  
+    if (this.forgotPasswordMode) {
+      this.loginForm.get('contrasena')?.clearValidators();
+    } else {
+      this.loginForm.get('contrasena')?.setValidators(Validators.required);
+    }
+  
+    this.loginForm.get('contrasena')?.updateValueAndValidity();
+  }
+
+  requestPasswordRecovery() {
+    if (this.loginForm.get('email')?.valid) {
+        this.isLoading = true;
+        
+        // URL según tipo de cuenta
+        const recoveryApiUrl = this.isCompanyLogin
+            ? "https://malo-backend-empresas.onrender.com/api/Recuperacion/solicitar-recuperacion"
+            : "https://malo-backend.onrender.com/api/Recuperacion/solicitar-recuperacion";
+
+        this.http.post(recoveryApiUrl, { email: this.loginForm.get('email')?.value }).subscribe(
+            () => {
+                this.isLoading = false;
+                this.successMessage = "Correo de recuperación enviado exitosamente.";
+                this.clearMessages();
+                setTimeout(() => this.toggleForgotPassword(), 7000);
+            },
+            () => {
+                this.isLoading = false;
+                this.errorMessage = "Error al enviar el correo de recuperación. Inténtelo más tarde.";
+                this.clearMessages();
+            }
+        );
+    }
+}
+
+  clearMessages() {
+    setTimeout(() => {
+      this.errorMessage = '';
+      this.successMessage = '';
+    }, 3000);
+  }
+
+  private handleLoginError(error: HttpErrorResponse) {
+    this.isLoading = false;
+    if (error.status === 401) {
+      // Verificar si el mensaje de la respuesta coincide con el mensaje de confirmación de correo
+      const errorMessage = error.error?.message;
+      if (errorMessage === "Debe confirmar su correo antes de iniciar sesión.") {
+        this.errorMessage = errorMessage;
+      } else {
+        this.errorMessage = "Credenciales inválidas. Por favor, verifique su correo y contraseña.";
+      }
+    }
+    if(error.status === 500){
+      this.errorMessage = "Credenciales inválidas. Por favor, verifique su correo y contraseña.";
+    }
+    else {
+      this.errorMessage = "Hubo un error. Intente de nuevo más tarde.";
+    }
+    this.clearMessages();
+  }
+}
 /*
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule, ValidatorFn,AbstractControl,ValidationErrors } from '@angular/forms';
 
